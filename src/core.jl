ab68392a9621a756bdf9a118edbaf66635df65fd
I.Mihara <issei.mihara@xforestx.com>
2025-11-21 00:22:46 +0900

    feat: add 'N' wildcard support for barcodes and update documentation.

--- src/core.jl
+++ src/core.jl
@@ -2,7 +2,7 @@
 Orchestrates the entire demultiplexing process for FASTQ files.
 Handles the preprocessing, dividing, demultiplexing, and merging of files.
 """
-function execute_demultiplexing(FASTQ_file1::String, FASTQ_file2::String, bc_file::String, output_dir::String; output_prefix1::String = "", output_prefix2::String = "", gzip_output::Union{Nothing, Bool} = nothing, max_error_rate::Float64 = 0.2, min_delta::Float64 = 0.0, mismatch::Int = 1, indel::Int = 1, classify_both::Bool = false, bc_complement::Bool = false, bc_rev::Bool = false, ref_search_range::String = "1:end", barcode_start_range::String = "1:end", barcode_end_range::String = "1:end")
+function execute_demultiplexing(FASTQ_file1::String, FASTQ_file2::String, bc_file::String, output_dir::String; output_prefix1::String = "", output_prefix2::String = "", gzip_output::Union{Nothing, Bool} = nothing, max_error_rate::Float64 = 0.2, min_delta::Float64 = 0.0, mismatch::Int = 1, indel::Int = 1, nindel::Union{Int, Nothing} = nothing, classify_both::Bool = false, bc_complement::Bool = false, bc_rev::Bool = false, ref_search_range::String = "1:end", barcode_start_range::String = "1:end", barcode_end_range::String = "1:end")
 	if !isdir(output_dir)
 		mkdir(output_dir)
 	end
@@ -22,7 +22,7 @@ function execute_demultiplexing(FASTQ_file1::String, FASTQ_file2::String, bc_fil
 	max_m   = maximum(ncodeunits.(bc_seqs))
 	ws      = SemiGlobalWorkspace(max_m)
 	
-	config = DemuxConfig(max_error_rate, min_delta, 0, mismatch, indel, classify_both, gzip_output, parse_dynamic_range(ref_search_range), parse_dynamic_range(barcode_start_range), parse_dynamic_range(barcode_end_range), bc_seqs, ids, ws)
+	config = DemuxConfig(max_error_rate, min_delta, 0, mismatch, indel, nindel, classify_both, gzip_output, parse_dynamic_range(ref_search_range), parse_dynamic_range(barcode_start_range), parse_dynamic_range(barcode_end_range), bc_seqs, ids, ws)
 
 	if workers == 1
 		classify_sequences(FASTQ_file1, FASTQ_file2, output_dir, output_prefix1, output_prefix2, config)
@@ -49,7 +49,7 @@ function execute_demultiplexing(FASTQ_file1::String, FASTQ_file2::String, bc_fil
 	end
 end
 
-function execute_demultiplexing(FASTQ_file::String, bc_file::String, output_dir::String; output_prefix::String = "", gzip_output::Union{Nothing, Bool} = nothing, max_error_rate::Float64 = 0.2, min_delta::Float64 = 0.0, mismatch::Int = 1, indel::Int = 1, bc_complement::Bool = false, bc_rev::Bool = false, ref_search_range::String = "1:end", barcode_start_range::String = "1:end", barcode_end_range::String = "1:end")
+function execute_demultiplexing(FASTQ_file::String, bc_file::String, output_dir::String; output_prefix::String = "", gzip_output::Union{Nothing, Bool} = nothing, max_error_rate::Float64 = 0.2, min_delta::Float64 = 0.0, mismatch::Int = 1, indel::Int = 1, nindel::Union{Int, Nothing} = nothing, bc_complement::Bool = false, bc_rev::Bool = false, ref_search_range::String = "1:end", barcode_start_range::String = "1:end", barcode_end_range::String = "1:end")
 	if !isdir(output_dir)
 		mkdir(output_dir)
 	end
@@ -66,7 +66,7 @@ function execute_demultiplexing(FASTQ_file::String, bc_file::String, output_dir:
 	max_m   = maximum(ncodeunits.(bc_seqs))
 	ws      = SemiGlobalWorkspace(max_m)
 
-	config = DemuxConfig(max_error_rate, min_delta, 0, mismatch, indel, false, gzip_output, parse_dynamic_range(ref_search_range), parse_dynamic_range(barcode_start_range), parse_dynamic_range(barcode_end_range), bc_seqs, ids, ws)
+	config = DemuxConfig(max_error_rate, min_delta, 0, mismatch, indel, nindel, false, gzip_output, parse_dynamic_range(ref_search_range), parse_dynamic_range(barcode_start_range), parse_dynamic_range(barcode_end_range), bc_seqs, ids, ws)
 
 	if workers == 1
 		classify_sequences(FASTQ_file, output_dir, output_prefix, config)
